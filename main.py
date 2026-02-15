de138adfa8f83cd3e6c88b65cc8875ca9e480d81
Abdullahanish <anishrajpoot245@gmail.com>
2026-02-15 15:13:49 +0500

    ml project 2 done

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
+import  pandas as pd 
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-import pandas as pd
-import numpy as np 
+from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
+import matplotlib.pyplot as plt
+import numpy as np
 
-# 1. Load data
-data = pd.read_csv("students.csv")
+#data loading from csv file 
+data=pd.read_csv("students.csv")
 
-# 2. Define Features and Target
-X = data[['Study_Hours']]
-Y = data['Final_Score']
+#input & output 
+X=data[['Study_Hours']]
+Y=data['Final_Score']
 
-# 3. Train the model
-model = LinearRegression()
-model.fit(X, Y)
+#object of Linearregression
+model=LinearRegression()
 
-# 4. Handle User Prediction
-user_input = float(input("Enter the number of hours a student study: "))
-# predict is a single value based on user input
-user_predict = model.predict([[user_input]])
+#training of data
+model.fit(X,Y)
+#modle pricdcition for all x values ,so that we can find mae,mse,rmse ,r2
+predict=model.predict(X)
 
-# 5. Handle Error Metrics (Predict for the WHOLE dataset to compare sizes)
-# We calculate 'all_predictions' so it has 100 values, matching Y's 100 values
-all_predictions = model.predict(X)
+mae=mean_absolute_error(Y,predict)
+mse=mean_squared_error(Y,predict)
+rmse=np.sqrt(mse)
+r2=r2_score(Y,predict)
 
-mae = mean_absolute_error(Y, all_predictions)
-mse = mean_squared_error(Y, all_predictions)
-rmse = np.sqrt(mse)
+print(" mae : ",mae)
+print("mse : ", mse)
+print("rmse : ",rmse)
+print(" r2 : ",r2)
 
-# 6. Results
-print(f"\nThe prediction for {user_input} hours is: {user_predict[0]:.2f}")
-print("-" * 30)
-print(f"Model Performance Metrics:")
-print(f"MAE  : {mae:.4f}")
-print(f"MSE  : {mse:.4f}")
-print(f"RMSE : {rmse:.4f}")
+#graphical representation of data
+# plt.figure(figsize=(10,6))
+# plt.hist(data['Final_Score'],bins=30,color='green',edgecolor='black')
+# plt.title("Final_Score")
+# plt.xlabel("along x_axis")
+# plt.ylabel("along y axis")
+# plt.grid(True)
+# plt.show()
+
+#user input
+hours=float(input("Enter the hours student study : "))
+#model pridiction for one value
+result=model.predict([[hours]])
+#result of the model means pridected value 
+print(result)
+
+
